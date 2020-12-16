7fbdc05f811f3c3acc098bdb99c0582cec3b6544
D'Artagnan von Pingel <dvonpingel@gmail.com>
2020-12-16 13:57:50 -0700

    final commit, finished MVP

--- index.js
+++ index.js
@@ -39,15 +39,30 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+  }
+  Person.prototype.eat = function(str) {
+    if (this.stomach.length >= 10) {
+      return 'go poop first, you are too full.';
+    } else {
+      this.stomach.push(str);
+    }
+  }
+  Person.prototype.poop = function() {
+    this.stomach.length = 0;
+  }
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`;
   }
  
  
 
   
   
-  
+
   
   
   /*
@@ -64,8 +79,14 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+  }
+  Car.prototype.fill = function(gallons) {
+    this.tank = this.tank + gallons;
   }
   
   
@@ -76,18 +97,23 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
+  }
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
   }
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. If 'this' is called in a global scope, it'll use the window object properties.
+    2. 'This' is called before the function, separated by a dot.
+    3. If called in a function, 'this' is referring to the object thats being created and returned.
+    4. 'This' is explicitly called when using call or apply.
   */
   
   
